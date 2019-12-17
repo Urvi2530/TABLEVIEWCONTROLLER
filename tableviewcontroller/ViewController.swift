6211c33b64bb2328a27dcca77bc56a0f55ace423
COE-05 <coe-05@coe-05s-imac.local>
2019-12-17 14:50:54 +0530

    Initial Commit

---
+++ tableviewcontroller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tableviewcontroller
+//
+//  Created by COE-05 on 17/12/19.
+//  Copyright © 2019 COE-05. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
